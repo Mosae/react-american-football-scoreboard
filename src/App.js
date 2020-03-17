462633fe8307e08acfbc939826fa206ebeb7446d
mosaelitsoane@gmail.com <mosaelitsoane@Mosaes-MacBook-Pro.local>
2020-03-17 07:49:10 -0500

    Finally got it started after compiling issues + added hooks

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 //TODO: STEP 1 - Import the useState hook.
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import BottomRow from "./BottomRow";
 
+
+console.log('hi');
 function App() {
   //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
+  const [homeScore , setHomeScore] = useState(0);
+  const [awayScore , setAwayScore] = useState(0);
+
 
   return (
+
+
     <div className="container">
       <section className="scoreboard">
         <div className="topRow">
@@ -15,7 +22,7 @@ function App() {
 
             {/* TODO STEP 3 - We need to change the hardcoded values in these divs to accept dynamic values from our state. */}
 
-            <div className="home__score">32</div>
+  <div className="home__score">{homeScore}</div>
           </div>
           <div className="timer">00:03</div>
           <div className="away">
@@ -37,6 +44,8 @@ function App() {
         </div>
       </section>
     </div>
+
+    
   );
 }
 
